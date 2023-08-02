817025ce21b415aa9c4eb857f0b5889733230908
Mike Wallick <mike@wallick.net>
2023-08-02 17:25:10 -0500

    Initial Commit

---
+++ BankZero/BankZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankZeroApp.swift
+//  BankZero
+//
+//  Created by Mike Wallick on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BankZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
